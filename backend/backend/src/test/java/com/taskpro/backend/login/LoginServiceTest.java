655f07663a2fb1a62a2e62e55c40e139659588d5
willen-gitonga <willengitonga.wg@gmail.com>
2023-02-01 11:45:14 +0300

    added new login service test

--- backend/backend/src/test/java/com/taskpro/backend/login/LoginServiceTest.java
+++ backend/backend/src/test/java/com/taskpro/backend/login/LoginServiceTest.java
@@ -4,11 +4,15 @@ import com.taskpro.backend.Users.User;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import static org.junit.Assert.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.mockito.Mockito.*;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,5 +29,11 @@ public class LoginServiceTest {
         LoginLogs loginLogs = new LoginLogs(user,new Date());
         service.saveLogs(loginLogs);
         verify(repository,times(1)).save(loginLogs);
+    }
+    @Test
+    public void getLogsTest(){
+        User user = new User("Santa","Clause","claus@gmail.com","axgznajkaaka",Role.USER);
+        when(repository.findAll()).thenReturn((List<LoginLogs>) Stream.of(new LoginLogs(user,new Date())).collect(Collectors.toList()));
+        assertEquals(1,service.getLogs().size());
     }
 }
